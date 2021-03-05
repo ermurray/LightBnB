ff8d3993eef076996c8b3d0308b8ea652f342c5d
ermurray <ermurray31@gmail.com>
2021-03-05 17:01:47 +0000

    added query for insert new property

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -95,7 +95,7 @@ const getAllProperties = function(options, limit = 10) {
   const qryParams = [];
   let qryStr = `SELECT properties.*, AVG(property_reviews.rating) AS average_rating
   FROM properties
-  JOIN property_reviews ON property_id = properties.id
+  LEFT JOIN property_reviews ON property_id = properties.id
   `;
 
   if (options.city) {
@@ -151,9 +151,12 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const qryStr = `INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
+  VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *`;
+  
+  const propertyValues = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code];
+  
+  return pool.query(qryStr, propertyValues)
+    .then(res => res.rows);
 };
 exports.addProperty = addProperty;
